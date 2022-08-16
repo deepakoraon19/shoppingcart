0b91d27ad9e143cbfbbadd807ecbe5be8849ac35
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-08-16 09:33:17 +0530

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState,useRef} from 'react'
 
-function App() {
+function App() {  
+  let product;
+  let data1 = useRef(null);
+  let [text, setText] = useState('')
+  const fetchData = async ()=>{
+    let res = await fetch("https://fakestoreapi.com/products")
+    let data = await res.json();
+    product = data;
+  }
+
+  useEffect(() => {   
+   fetchData().then(()=>{console.log(product)}
+   )
+  },[])
+ 
+  const debounce = (func)=>{
+    let timer;
+    console.log("debounce")   
+    return function (...args){
+      if(timer)clearTimeout(timer)
+      
+      timer = setTimeout(()=>{func(...args)},1000);
+    }
+  }
+  const print = ()=>{     
+    // console.log(data1.value) 
+      setText(data1.current.value)
+  }
+  
+  const handleChange = debounce((e)=>{print()})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
